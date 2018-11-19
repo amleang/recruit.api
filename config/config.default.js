ba60f6199a8587b11287c7e350e8936d247ee3ea
少游 <shaoyou@jzez100.com>
2018-11-19 22:39:26 +0800

    update

--- config/config.default.js
+++ config/config.default.js
@@ -29,7 +29,8 @@ module.exports = appInfo => {
   config.mysql = {
     client: {
       // host
-      host: '118.25.98.91',
+      //host: '118.25.98.91',
+      host:"127.0.0.1",
       // 端口号
       port: '3306',
       // 用户名
